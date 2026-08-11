7eba3e6a4cf6b0ce9f0c17cf6a85084cf868ff2d
samikshapadghan-del <samikshapadghan@gmail.com>
2026-08-11 21:38:05 +0530

    Switch to ChatGroq and update response handling

--- Conversational-Chatbot-using-Langchain-main/app.py
+++ Conversational-Chatbot-using-Langchain-main/app.py
@@ -1,34 +1,30 @@
 import os
 import streamlit as st
-
 from langchain.schema import HumanMessage, SystemMessage, AIMessage
-from langchain.chat_models import ChatOpenAI
-
-st.set_page_config(page_title="Converational Chatbot", page_icon=":earth_americas:")
-
-st.header("Hey, Lets Chat!")
-
+from langchain_groq import ChatGroq
 from dotenv import load_dotenv
+
 load_dotenv()
 
-chat = ChatOpenAI(temperature=0.5)
+st.set_page_config(page_title="Conversational Chatbot", page_icon=":earth_americas:")
+st.header("Hey, Lets Chat!")
+
+chat = ChatGroq(temperature=0.5, model_name="llama-3.3-70b-versatile", groq_api_key=os.environ["GROQ_API_KEY"])
 
 if 'flowmessages' not in st.session_state:
     st.session_state['flowmessages'] = [
         SystemMessage(content="Hello, I am a chatbot. I am here to help you with your queries. Please ask me anything!")
     ]
 
-def get_openai_response(query):
+def get_response(query):
     st.session_state['flowmessages'].append(HumanMessage(content=query))
     answer = chat(st.session_state['flowmessages'])
     st.session_state['flowmessages'].append(AIMessage(content=answer.content))
-
     return answer.content
 
 input = st.text_input("Input: ", key="input")
-response = get_openai_response(input)
-
 submit = st.button("Submit")
 
-if submit:
+if submit and input:
+    response = get_response(input)
     st.write(response)
